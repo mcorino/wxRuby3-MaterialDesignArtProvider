5df7ef7744bd675d65257198ccecd74395a412b1
Martin Corino <mcorino@m2c-software.nl>
2024-05-16 15:27:41 +0200

    add sudo

--- tools/scripts/setup-ubuntu.sh
+++ tools/scripts/setup-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y git make gcc gpg xvfb xfonts-75dpi curl procps
-apt-get install -y 'libgtk-3-[0-9]+' 'libwebkit2gtk-4.[0-9]-[0-9]+' 'libgspell-1-[0-9]+' libnotify4 'libsecret-1-[0-9]+' curl
+sudo apt-get update
+sudo apt-get install -y git make gcc gpg xvfb xfonts-75dpi curl procps
+sudo apt-get install -y 'libgtk-3-[0-9]+' 'libwebkit2gtk-4.[0-9]-[0-9]+' 'libgspell-1-[0-9]+' libnotify4 'libsecret-1-[0-9]+' curl
